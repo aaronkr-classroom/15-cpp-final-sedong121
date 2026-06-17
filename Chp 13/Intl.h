cc259950a18ab0efb8d52cc341f6b3a7d0c5f0ec
ththfql <145094239+ththfql@users.noreply.github.com>
2026-06-17 15:40:17 +0900

    finish

---
+++ Chp 13/Intl.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#ifndef GUARD_Intl_h
+#define GUARD_Intl_h
+
+#include <iostream>
+
+#include "Core.h"
+
+using namespace std;
+
+class Intl : public Core {
+public:
+    Intl() : toeic(0) {}
+    Intl(istream& is) { read(is); }
+
+    double grade() const;
+    istream& read(istream&);
+
+private:
+    double toeic;    // TOEIC 점수
+
+    Intl* clone() const { return new Intl(*this); }
+};
+
+#endif
